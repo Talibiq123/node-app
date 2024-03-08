12a69611e18636f94c2166ddc5dc364ec9db802a
Talibiq123 <talibsaeed90@gmail.com>
2024-03-08 21:27:29 +0530

    File Module

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // console.log("Hello world!");
 const lib = require("./lib.js");
 // import {sum, diff} from "./lib.js";
+const fs = require("fs");
+
+const t1 = performance.now();
+const text = fs.readFileSync('demo.txt', 'utf-8')
+
+// fs.readFile('demo.txt', 'utf-8', (err, txt) => {
+//     console.log(txt);
+// })
+// console.log(text);
+
 
 console.log(lib.sum(2, 3), lib.diff(10, 5));
 // console.log(sum(2, 3), diff(10, 5));
 
 const a = 5;
 console.log(a);
+const t2 = performance.now();
+console.log(t2-t1);
 
 // function diff(a, b) {
 //     return a - b;
